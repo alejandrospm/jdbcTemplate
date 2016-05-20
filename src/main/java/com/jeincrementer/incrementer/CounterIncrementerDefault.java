377410612b88593b53b1703933d48da60f66ff5e
Sergio Pedraza <alejandrospm@gmail.com>
2016-05-20 16:40:06 -0500

    Added logging

--- src/main/java/com/jeincrementer/incrementer/CounterIncrementerDefault.java
+++ src/main/java/com/jeincrementer/incrementer/CounterIncrementerDefault.java
@@ -2,6 +2,8 @@ package com.jeincrementer.incrementer;
 
 import java.util.Optional;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
@@ -11,15 +13,19 @@ import com.jeincrementer.model.JEStats;
 
 @Service
 public class CounterIncrementerDefault {
-	
+
+	private static final Logger LOGGER = LogManager.getLogger(JEStatsDao.class);
+
 	@Autowired
 	private JEStatsDao jEStatsDao;
-	
+
 	public static final long MAX_HEADER_COUNTER_VALUE = 1_500;
 
 	public long getHeaderCounter() {
-		
+
 		JEStats jeStats = getJEStatsFirstObjectFromPersistence();
+		LOGGER.info("Retrieved Data: HEADER COUNTER = {}, LINE COUNTER = {}", jeStats.getHeaderCounter(),
+				jeStats.getLineCounter());
 		long newHeaderCounter = getNewHeaderCounter(jeStats.getHeaderCounter());
 		jeStats.setHeaderCounter(newHeaderCounter);
 		jEStatsDao.update(jeStats);
@@ -28,11 +34,10 @@ public class CounterIncrementerDefault {
 
 	private long getNewHeaderCounter(final long currentHeaderCounter) {
 		long headerCounter = currentHeaderCounter;
-		if(validateIfHeaderShouldBeReseted(headerCounter)){
+		if (validateIfHeaderShouldBeReseted(headerCounter)) {
 			headerCounter = 1;
-		}
-		else{
-			headerCounter +=1;
+		} else {
+			headerCounter += 1;
 		}
 		return headerCounter;
 	}
@@ -43,16 +48,17 @@ public class CounterIncrementerDefault {
 
 	private JEStats getJEStatsFirstObjectFromPersistence() {
 		Optional<JEStats> jeStatsOptional = getFirstCountersFromPreviousData();
-		if(!jeStatsOptional.isPresent()){
+		if (!jeStatsOptional.isPresent()) {
 			jeStatsOptional = Optional.of(createJEStatusInitialData());
 		}
 		return jeStatsOptional.get();
 	}
-	
-	private Optional<JEStats> getFirstCountersFromPreviousData(){
-		try{
+
+	private Optional<JEStats> getFirstCountersFromPreviousData() {
+		try {
 			return Optional.ofNullable(jEStatsDao.getFirstElement());
-		} catch (DataAccessException dae){
+		} catch (DataAccessException dae) {
+			LOGGER.error(dae.getMessage());
 			return Optional.empty();
 		}
 	}
@@ -65,11 +71,13 @@ public class CounterIncrementerDefault {
 
 	public long getLineCounter() {
 
-		JEStats jeStats= getJEStatsFirstObjectFromPersistence();
+		JEStats jeStats = getJEStatsFirstObjectFromPersistence();
+		LOGGER.info("Retrieved Data: HEADER COUNTER = {}, LINE COUNTER = {}", jeStats.getHeaderCounter(),
+				jeStats.getLineCounter());
 		long newLineCounterValue = jeStats.getLineCounter() + 1;
 		jeStats.setLineCounter(newLineCounterValue);
 		jEStatsDao.update(jeStats);
 		return newLineCounterValue;
 	}
-	
+
 }
